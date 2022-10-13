86017324c2b5c4be0383f08f90ee99c82d00dfa6
Iara Pou <iarapou21@hotmail.com>
2022-10-12 21:56:45 -0300

    Crea un event handler

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const $botonCalcular = document.querySelector("#boton-calcular");
 
+$botonCalcular.onclick= function(){
     const cantidadEntradas = document.querySelector("#cantidad");
     const categoria = document.querySelector("#categoria");
     let $precio = document.querySelector("#precio"); 
