91d584b8b8e7ef44b9bed88a43e9b0cda30bbc7c
Iara Pou <iarapou21@hotmail.com>
2022-10-12 21:56:00 -0300

    Crea variables principales JS
    Referencié las variables que van a ser necesarias para el cálculo (con $ si se muestran en pantalla y sin esto si son un valor que voy a tomar en la operación)

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const $botonCalcular = document.querySelector("#boton-calcular");
+
+    const cantidadEntradas = document.querySelector("#cantidad");
+    const categoria = document.querySelector("#categoria");
+    let $precio = document.querySelector("#precio"); 
+
+}
