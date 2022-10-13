f6bd60f061aadb2ddf0b27e9fd9c49244aac3372
Iara Pou <iarapou21@hotmail.com>
2022-10-13 10:00:33 -0300

    Suma función que devuelve el porcentaje
    Cree una función que tome la categoría del ticket y devuelva el porcentaje indicado en mi script de funciones para mantener el orden y sintetizar el otro script que probablemente va a tener validaciones.

--- js/funciones.js
+++ js/funciones.js
@@ -1,8 +1,24 @@
-function calcularPorcentajeDescuento(porcentaje){
+function devolverPorcentajeSegunCategoria(categoria) {
+    let porcentaje;
+    switch (categoria) {
+        case "Estudiante":
+            porcentaje = 80;
+            break;
+        case "Trainee":
+            porcentaje = 50;
+            break;
+        case "Junior":
+            porcentaje = 15;
+            break;
+            //validación en switch o en script origen?
+    }
+    return porcentaje;
+}
+function calcularPorcentajeDescuento(porcentaje) {
     return (PRECIO_INICIAL * porcentaje) / 100;
 }
-function calcularPrecioEntradas(cantidad, descuento){
-    return (PRECIO_INICIAL - descuento)* cantidad;
+function calcularPrecioEntradas(cantidad, descuento) {
+    return (PRECIO_INICIAL - descuento) * cantidad;
 }
 
 const PRECIO_INICIAL = 200
