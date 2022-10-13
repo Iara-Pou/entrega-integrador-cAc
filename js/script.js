3347a2e592f0289cb4f9d83d25f1f0b19ba7f726
Iara Pou <iarapou21@hotmail.com>
2022-10-12 22:21:46 -0300

    Suma propiedad value
    Cuando el usuario hace click en el botón, se lee el contenido de las constantes. ahorra un par de líneas de código.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const $botonCalcular = document.querySelector("#boton-calcular");
 
 $botonCalcular.onclick= function(){
-    const cantidadEntradas = document.querySelector("#cantidad");
-    const categoria = document.querySelector("#categoria");
+    const cantidadEntradas = document.querySelector("#cantidad").value;
+    const categoria = document.querySelector("#categoria").value;
     let $precio = document.querySelector("#precio"); 
+
+    
 
     return false;
 }
