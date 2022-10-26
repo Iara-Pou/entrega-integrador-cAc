18050a6d4c7133d786c1974e58a1b1c38f7e921f
Iara Pou <iarapou21@hotmail.com>
2022-10-26 10:47:34 -0300

    Suma mensaje de compra
    Muestra un div con el mensaje de que se completaron los datos correctamente, con dos botones. uno de compra y otro para cancelar la operación.

--- js/scriptFormulario.js
+++ js/scriptFormulario.js
@@ -1,15 +1,17 @@
 const $botonCalcular = document.querySelector("#boton-calcular");
 
 $botonCalcular.onclick= function(){
+    let $titulo = document.querySelector("h1");
+    const $nombre = document.querySelector("#nombre").value;
     const cantidadEntradas = document.querySelector("#cantidad").value;
     const categoria = document.querySelector("#categoria").value;
-    let $titulo = document.querySelector("h1");
-    let $precio = document.querySelector("#precio"); 
-    const $nombre = document.querySelector("#nombre").value;
 
-    if (cantidadEntradas>0 && categoria !== "" && $nombre !== ""){
+    let esExito = cantidadEntradas>0 && categoria !== "" && $nombre !== "";
+
+    if (esExito){
         let $formulario = document.querySelector("form");
         let $mensajeFinal = document.querySelector("#mensaje-final")
+        let $precio = document.querySelector("#precio"); 
         let descuento = calcularPorcentajeDescuento(devolverPorcentajeSegunCategoria(categoria));
         
         $titulo.textContent = `¡Gracias por participar, ${$nombre}!`
@@ -18,7 +20,6 @@ $botonCalcular.onclick= function(){
         $formulario.classList.add("oculto");
         $mensajeFinal.classList.remove("oculto");
 
-        
     } else if (cantidadEntradas<=0) {
         alert("¡Debes ingresar 1 o más entradas!")
     } else if (categoria===""){
@@ -29,3 +30,4 @@ $botonCalcular.onclick= function(){
 
     return false;
 }
+
