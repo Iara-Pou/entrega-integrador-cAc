d9603de59210987c2ede16d7d25563925f03e0c6
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2025-01-18 23:48:56 -0300

    Suma mejoras formularios a proyecto en prod

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,38 +1,113 @@
 const $botonEnviar = document.querySelector("#botonEnvio");
+$botonEnviar.onclick = (e) => {
+  e.preventDefault();
+  verificarInputs();
+};
 
-$botonEnviar.onclick= function(){
-    const $nombre = document.querySelector("#nombre").value;
-    let $mensaje = document.querySelector("#mensaje"); 
+function verificarInputs() {
+  const $inputNombre = document.querySelector("#nombre");
+  const $inputApellido = document.querySelector("#apellido");
+  const $inputTemaCharla = document.querySelector("#temaCharla");
 
-        if($nombre.trim() !== ""){
-            $mensaje.textContent = `¡Gracias por participar, ${$nombre}! En brevedad nos contactaremos.`
+  //almaceno el input y su error
+  const inputErrores = {
+    nombre: [$inputNombre, validarNombreApellido($inputNombre.value)],
+    apellido: [$inputApellido, validarNombreApellido($inputApellido.value)],
+    temaCharla: [$inputTemaCharla, validarTemaCharla($inputTemaCharla.value)],
+  };
 
-            let $elementosQueOcultar = document.querySelectorAll(".elemento-a-ocultar")
-            $elementosQueOcultar.forEach(row => row.classList.toggle('oculto'));
-        }
+  const esExito = contarErrores(inputErrores) === 0;
 
-
-    return false;
+  if (esExito) {
+    let $mensaje = document.querySelector("#mensaje");
+    $mensaje.textContent = `¡Gracias por participar, ${$inputNombre.value}! En brevedad nos contactaremos.`;
+    let $elementosQueOcultar = document.querySelectorAll(".elemento-a-ocultar");
+    $elementosQueOcultar.forEach((row) => row.classList.toggle("oculto"));
+  } else {
+    borrarErroresAnteriores();
+    mostrarErrores(inputErrores);
+  }
 }
 
-function scrollearArriba (){
-    document.documentElement.scrollTop = 0;
-    document.body.scrollTop = 0;
+function borrarErroresAnteriores() {
+  let $errores = document.querySelectorAll(".error");
+  $errores.forEach((error) => error.remove());
+  let $inputsErrores = document.querySelectorAll(".input-error");
+  $inputsErrores.forEach((input) => input.classList.remove("input-error"));
+}
+
+function contarErrores(errores) {
+  let contador = 0;
+  Object.values(errores).forEach((error) => {
+    //si el texto de error no está vacío (campo 1)
+    if (error[1] !== "") {
+      contador++;
     }
+  });
+  return contador;
+}
+
+function mostrarErrores(errores) {
+  Object.values(errores).forEach((error) => {
+    if (error[1] !== "") {
+      //modifica input con error
+      error[0].classList.add("input-error");
+      //maneja texto con error
+      const TEXTO_ERROR = error[1];
+      let $labelError = document.createElement("label");
+      $labelError.textContent = TEXTO_ERROR;
+      $labelError.classList.add("error");
+      error[0].insertAdjacentElement("afterend", $labelError);
+    }
+  });
+}
+
+function esconderFormulario() {
+  $formulario.classList.add("oculto");
+}
+
+function validarNombreApellido(nombre) {
+  if (nombre.trim() === "") {
+    return "El campo debe contener por lo menos un carácter.";
+  }
+  if (!/^[a-zA-Z]+$/.test(nombre)) {
+    return "El campo debe contener solo letras.";
+  }
+  if (nombre.length > 20) {
+    return "El campo debe tener menos de 20 carácteres";
+  }
+
+  return "";
+}
+
+function validarTemaCharla(tema) {
+  if (tema.trim() === "") {
+    return "El campo debe contener por lo menos un carácter.";
+  }
+  if (tema.length > 100) {
+    return "El campo debe tener menos de 100 carácteres.";
+  }
+  return "";
+}
+
+// Funcionalidad de widget scroll top
+function scrollearArriba() {
+  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+}
 
 const $botonGoTop = document.querySelector("#go-top-container");
 $botonGoTop.onclick = scrollearArriba;
 
 const altoDePantalla = window.innerHeight;
-const ALTURA_MOSTRAR_BOTON = altoDePantalla/5.5
+const ALTURA_MOSTRAR_BOTON = altoDePantalla / 5.5;
 
-window.addEventListener('scroll', () =>{
-    if(window.scrollY < ALTURA_MOSTRAR_BOTON){
-        $botonGoTop.classList.add("oculto");
-        $botonGoTop.classList.remove("fade-in")
-    } else {
-        $botonGoTop.classList.remove("oculto");
-        $botonGoTop.classList.add("fade-in");
-    }
-}
-);
+window.addEventListener("scroll", () => {
+  if (window.scrollY < ALTURA_MOSTRAR_BOTON) {
+    $botonGoTop.classList.add("oculto");
+    $botonGoTop.classList.remove("fade-in");
+  } else {
+    $botonGoTop.classList.remove("oculto");
+    $botonGoTop.classList.add("fade-in");
+  }
+});
